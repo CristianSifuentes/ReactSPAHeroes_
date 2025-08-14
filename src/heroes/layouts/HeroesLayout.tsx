943fde808febbe3647090618adbb819e50341278
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-08-13 19:41:22 -0600

    Add information for lazy load

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,10 +1,26 @@
-import { Outlet } from "react-router"
+import { Link, Outlet } from "react-router"
 
 export const HeroesLayout = () => {
   return (
-    <div className="bg-red-500 p-4">
-        <h1>Hi from HeroesLayout</h1>
-        <Outlet></Outlet>
+    <div className="bg-red-100 p-4">
+        <ul>
+          
+           <li>
+            <Link to="/heroes">Home</Link>
+          </li>
+          <li>
+            <Link to="/heroes/:id">Hero</Link>
+            </li>
+          <li>
+            <Link to="/heroes/search">Search</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin</Link>
+          </li>
+        </ul>
+        <section className="mt-10">
+          <Outlet></Outlet>
+        </section>
     </div>
   )
 }
